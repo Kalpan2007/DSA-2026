1180df40c7d2bd47ee654b666942536dd354036c
Kalpan2007 <kalpankaneriya@gmail.com>
2026-01-12 10:08:19 +0530

    add daily question 12 jan

---
+++ January/3rd Week/12 jan 2026/1266.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& p) {
+        int n = p.size();
+        int ans =0;
+        for(int i = 0;i<n-1;i++){
+             int x1 = p[i][0];
+             int y1 = p[i][1];
+             int x2 = p[i+1][0];
+             int y2 = p[i+1][1];
+             int dy = abs(y2-y1);
+             int dx = abs(x2-x1);
+             ans += min(dy,dx) + abs(dx-dy);
+
+        }
+        return ans;
+    }
+};
