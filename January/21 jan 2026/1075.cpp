0b1acb56bbcfc0a63df3ea6a64cd42b8a2ffe008
Kalpan2007 <kalpankaneriya@gmail.com>
2026-01-21 22:08:25 +0530

    Solve Sql-50 question

---
+++ January/21 jan 2026/1075.cpp
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select p.project_id,round(ifnull(avg(e.experience_years),0),2) as average_years 
+from project 
+as p 
+left join employee as e 
+on p.employee_id=e.employee_id 
+group by p.project_id;
