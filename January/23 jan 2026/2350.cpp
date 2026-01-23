a4516b36b0ec341429b352ecede33a1548df4d4f
Kalpan2007 <kalpankaneriya@gmail.com>
2026-01-23 19:40:36 +0530

    solve leetcode-75 question

---
+++ January/23 jan 2026/2350.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        int n = grid.size();
+        map<vector<int>, int> rowMap; 
+        
+        for (auto &row : grid) rowMap[row]++;
+
+        int ans = 0;
+        for (int c = 0; c < n; c++) {
+            vector<int> col;
+            for (int r = 0; r < n; r++) col.push_back(grid[r][c]);
+            if (rowMap.count(col)) ans += rowMap[col];
+        }
+        return ans;
+    }
+};
