693b89e2a884378dfbc4773b7c3f1060d4e08aba
Kalpan2007 <kalpankaneriya@gmail.com>
2026-01-30 20:57:14 +0530

    Solve S-50 Question

---
+++ January/30 jan 2026/1070.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select s.product_id , s.year 
+as first_year , s.quantity , s.price
+from Sales s 
+join (
+    select product_id , min(year) 
+    as first_year
+    from Sales
+    group by product_id
+) f
+on s.product_id = f.product_id
+AND s.year = f.first_year;
