3f1a21958eeb7bc9ec4077bbdadeb96e3999a3bc
Kalpan2007 <kalpankaneriya@gmail.com>
2026-01-31 13:54:18 +0530

    Update Code Of 394

--- January/31 jan 2026/394.cpp
+++ January/31 jan 2026/394.cpp
@@ -14,8 +14,9 @@ public:
                     curr_str = st.top() + curr_str ;
                     st.pop();
                 }
-                
+                cout << st.top() << endl;
                 st.pop();
+                cout << st.top() << endl;
                 string number = "";
                 
                 
@@ -24,6 +25,7 @@ public:
                     st.pop();
                 }
                 int k_time = stoi(number);   
+                cout << k_time << endl;
                 
                 while(k_time--){
                     for(int p = 0; p < curr_str.size() ; p++)
