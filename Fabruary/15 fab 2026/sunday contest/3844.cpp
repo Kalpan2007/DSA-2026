37096a96bbdfc24394fa2422a73b9f01d4725eff
Kalpan2007 <kalpankaneriya@gmail.com>
2026-02-16 17:43:35 +0530

    3rd question of contest

--- Fabruary/15 fab 2026/sunday contest/3844.cpp
+++ Fabruary/15 fab 2026/sunday contest/3844.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    vector<int> toggleLightBulbs(vector<int>& b) {
-        bool s[101] = {false};
-        vector<int> r;
+    bool p(string& t , int i , int j){
+        for(; i < j;i++ , j--){
+            if(t[i]!=t[j]) return 0;
 
-        for(int x : b){
-            s[x] =! s[x];
         }
-            for(int i = 0;i<= 100;i++){
-                if(s[i]){
-                    r.push_back(i);
+                    return 1;
+    }
+    int almostPalindromic(string s) {
+        int n = s.size();
+        int i , j , l , r;
+        for(int len = n ; len >=2 ; len--){
+            for(i = 0;i<=n-len;i++){
+                j = i + len -1;
+                l=i;
+                r=j;
+                while(l < r && s[l]==s[r]){
+                    l++;
+                    r--;
+                }
+                if(l >=r || p(s ,l+1 , r) || p(s , l , r-1)){
+                    return len;
                 }
             }
-        return r;
+        }
+        return 0;
     }
 };
