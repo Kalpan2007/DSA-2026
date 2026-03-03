5408d2f9ba6e8e477edbbb15a302adc6166c474e
Kalpan2007 <kalpankaneriya@gmail.com>
2026-03-03 15:48:11 +0530

    Solve L-75 Third Question

---
+++ March/3 mar 2026/452.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if (points.empty()) return 0;
+        sort(points.begin(), points.end(),[](const vector<int>& a, const vector<int>& b) {
+                 return a[1] < b[1];
+            }
+        );
+
+        int ans = 1;
+        long long lastEnd = points[0][1];
+
+        for (int i = 1; i < points.size(); i++) {
+            if (points[i][0] > lastEnd) {
+                ans++;
+                lastEnd = points[i][1];
+            }
+        }
+
+        return ans;
+    }
+};
